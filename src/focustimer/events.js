9edcb964e4303cf9c59ec5ec42e38cc0354c108b
dafine lira <dafinelirar@gmail.com>
2024-07-01 20:27:42 -0300

    Alterando o estado da aplicação conforme a ação

--- src/focustimer/events.js
+++ src/focustimer/events.js
@@ -8,6 +8,7 @@ export function registerControls () {
             return
         }
 
+        actions[action]()
         
     })
 }
--- src/focustimer/index.js
+++ src/focustimer/index.js
@@ -4,4 +4,6 @@ import * as events from './events.js'
 export function start(minutes,seconds) {
     state.minutes = minutes
     state.seconds = seconds
+
+    events.registerControls ()
 }
